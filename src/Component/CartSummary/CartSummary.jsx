f058877e78b7a875786e92c6a44ce23afad68f9c
Sowrub mojumder <nazmulhasun7@gmail.com>
2023-03-28 09:23:20 -0700

    local storage done

--- src/Component/CartSummary/CartSummary.jsx
+++ src/Component/CartSummary/CartSummary.jsx
@@ -5,18 +5,27 @@ const CartSummary = ({cart}) => {
     // console.log(props.cart);
     // const cart =props.cart;
     // const {cart}=props;
+    // console.log(cart);
    let TotalPrice=0;
    let TotalShipping=0;
+   let quantity=0;
    for (const product of cart) {
-    TotalPrice +=product.price
+    // same kaj
+    if (product.quantity === 0) {
+        product.quantity= 1;
+    }
+    // or
+    // product.quantity =product.quantity || 1;
+    TotalPrice = TotalPrice + product.price *product.quantity;
     TotalShipping +=product.shipping
+    quantity =quantity + product.quantity;
    }
    const text=TotalPrice*5/100;
   const GrendTotal=TotalPrice+TotalShipping+text;
     return (
-        <div className='cart'>
+        <div className='allCart'>
             <h3 className='summary'>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${TotalPrice.toFixed(2)}</p>
             <p>Total Shipping Charge: ${TotalShipping.toFixed(2)}</p>
             <p>Text: ${text.toFixed(2)} </p>
--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -3,17 +3,38 @@ import './Shop.css'
 
 import Cart from '../Cart/Cart';
 import CartSummary from '../CartSummary/CartSummary';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 const Shop = () => {
-    const [products, setProducts]=useState([]);
+    const [products, setProducts]= useState([]);
     const [cart, setCart]=useState([]);
     useEffect(()=>{
         fetch(`products.json`)
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+
+    useEffect(()=>{
+      const displayShoppingCart = getShoppingCart();
+      let addProductsData=[];
+      // console.log(displayShoppingCart);
+      for(const id in displayShoppingCart) {
+        
+          const addProducts=products.find((product)=>product.id ===id);
+          // console.log(addProducts);
+          if(addProducts){
+            const quantity=displayShoppingCart[id];
+            addProducts.quantity=quantity;
+            addProductsData.push(addProducts)
+            console.log(addProducts);
+          }
+      }
+      setCart(addProductsData)
+    }, [products])
+
     const AddToCart=(product)=>{
         const newCart=[...cart,product];
         setCart(newCart);
+        addToDb(product.id)
       }
     // console.log(products);
     return (
